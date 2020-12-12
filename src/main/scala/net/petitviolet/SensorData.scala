cb0abd26c57f7710e6a5d82205f7a3e4a06c7371
petitviolet <violethero0820@gmail.com>
2020-12-12 22:13:06 +0900

    extracted SensorData

---
+++ src/main/scala/net/petitviolet/SensorData.scala
@@ -0,0 +1,50 @@
+package net.petitviolet
+
+import org.apache.flink.api.common.serialization.SerializationSchema
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode
+
+import scala.util.{ Failure, Success, Try }
+
+object SensorData {
+  def deserialize(obj: ObjectNode): Option[SensorData] = {
+    Try {
+      val value = obj.get("value")
+      SensorData(
+        value.get("deviceId").asLong(),
+        value.get("temperature").asDouble(),
+        value.get("humidity").asDouble(),
+      )
+    } match {
+      case Success(x) => Some(x)
+      case Failure(exception) => {
+        println(s"failed to parse JSON. obj = $obj, exception = $exception")
+        None
+      }
+    }
+  }
+
+}
+case class SensorData(deviceId: Long, temperature: Double, humidity: Double)
+
+object SensorAggregatedResult {
+  val serializationSchema: SerializationSchema[SensorAggregatedResult] = ???
+}
+
+case class SensorAggregatedResult(deviceId: Long, count: Int, temperatureList: Seq[Double], humidityList: Seq[Double]) {
+  private val avgTemperature = temperatureList.sum / count
+  private val avgHumidity = humidityList.sum / count
+  private val map: Map[String, Any] = (
+    ("deviceId", deviceId)
+      :: ("count", count)
+      :: ("temperatureList", temperatureList)
+      :: ("avgTemperatures", avgTemperature)
+      :: ("humidityList", humidityList)
+      :: ("avgHumidity", avgHumidity)
+      :: Nil
+    ).toMap
+
+  override def toString: String = {
+    // serialization
+    util.parsing.json.JSONObject(map).toString()
+  }
+}
