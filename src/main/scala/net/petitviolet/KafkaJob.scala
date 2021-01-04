30c8f98d10badd2239a25a4551a524c849d87278
petitviolet <violethero0820@gmail.com>
2021-01-04 22:52:18 +0900

    Implemented WatermarkStrategy

--- src/main/scala/net/petitviolet/KafkaJob.scala
+++ src/main/scala/net/petitviolet/KafkaJob.scala
@@ -1,13 +1,13 @@
 package net.petitviolet
 
-import java.time.Duration
+import java.time.LocalDateTime
 import java.util.Properties
 
 import org.apache.flink.api.common.eventtime.{ SerializableTimestampAssigner, WatermarkStrategy }
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.java.utils.ParameterTool
-import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
 import org.apache.flink.runtime.state.StateBackend
+import org.apache.flink.runtime.state.memory.MemoryStateBackend
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.datastream.DataStreamSink
@@ -30,38 +30,54 @@ object KafkaJob {
     // set up the execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-     env.getConfig.setAutoWatermarkInterval(1000L)
+    env.setStateBackend(new MemoryStateBackend: StateBackend)
 
-    val stateBackend: StateBackend = new RocksDBStateBackend("file:///flink/states")
-    env.setStateBackend(stateBackend)
-
-    // $ ../flink-1.11.2/bin/flink run -c net.petitviolet.Job2 \
+    // $ ../flink-1.11.2/bin/flink run -c net.petitviolet.KafkaJob \
     //     ./target/scala-2.12/flink_practice-assembly-0.1-SNAPSHOT.jar \
     //     --bootstrap.servers localhost:32771 \
     //     --kafka-topic-in flink-topic-in \
     //     --kafka-topic-out flink-topic-out \
     //     --group.id test
     val parameterTool = ParameterTool.fromArgs(args)
-    val properties = new Properties()
-    properties.setProperty("bootstrap.servers", parameterTool.getRequired("bootstrap.servers"))
-    // properties.setProperty("group.id", parameterTool.getRequired("group.id"))
 
-    val kafkaStream: DataStream[ObjectNode] = env.addSource(
+    val source = {
+      val sourceProperties = new Properties()
+      sourceProperties.setProperty("bootstrap.servers", parameterTool.getRequired("bootstrap.servers"))
+      sourceProperties.setProperty("group.id", parameterTool.getRequired("group.id"))
+
       new FlinkKafkaConsumer(parameterTool.getRequired("kafka-topic-in"),
         new JSONKeyValueDeserializationSchema(false),
-        properties,
+        sourceProperties,
       )
-    )
-    val timestampAssigner = new SerializableTimestampAssigner[SensorData] {
-      override def extractTimestamp(element: SensorData, recordTimestamp: Long): Long = element.timestamp getOrElse recordTimestamp
     }
 
-    val periodicAssigner: WatermarkStrategy[SensorData] =
-      WatermarkStrategy.forMonotonousTimestamps() //Duration.ofSeconds(1L))
-        .withTimestampAssigner(timestampAssigner)
+    val sink = {
+      val sinkProperties = new Properties()
+      sinkProperties.setProperty("bootstrap.servers", parameterTool.getRequired("bootstrap.servers"))
+      sinkProperties.setProperty("group.id", parameterTool.getRequired("group.id"))
+
+      new FlinkKafkaProducer(
+        parameterTool.getRequired("kafka-topic-out"),
+        new SimpleStringSchema(),
+        sinkProperties
+      )
+    }
+    val kafkaStream: DataStream[ObjectNode] = env.addSource(source)
+
+    val watermarkStrategy: WatermarkStrategy[SensorData] = {
+      val timestampAssigner = new SerializableTimestampAssigner[SensorData] {
+        override def extractTimestamp(element: SensorData, recordTimestamp: Long): Long = {
+          println(s"${LocalDateTime.now()}[extractTimestamp]element: $element, recordTimestamp: $recordTimestamp")
+          element.timestamp getOrElse recordTimestamp
+        }
+      }
+
+      WatermarkStrategy.forMonotonousTimestamps().withTimestampAssigner(timestampAssigner)
+    }
 
     val deserializedStream: DataStream[SensorData] = kafkaStream.flatMap { SensorData.deserialize(_) }
-      .assignTimestampsAndWatermarks(periodicAssigner)
+      .name("deserializedStream")
+      .assignTimestampsAndWatermarks(watermarkStrategy)
 
     val processed: DataStream[SensorAggregatedResult] = deserializedStream.keyBy { _.deviceId }
       .timeWindow(Time.seconds(10))
@@ -80,15 +96,14 @@ object KafkaJob {
           }
         }
       }
+      .name("processed")
 
     val processedStrings: DataStream[String] = processed.map { result => result.serialize }
+      .name("processedString")
 
-    val graph: DataStreamSink[String] = processedStrings.addSink(new FlinkKafkaProducer(
-      parameterTool.getRequired("bootstrap.servers"),
-      parameterTool.getRequired("kafka-topic-out"),
-      new SimpleStringSchema()
-    ))
+    val graph: DataStreamSink[String] = processedStrings.addSink(sink).name("graph")
 
+    graph
     processed.print()
 
     // execute program
